a3647cb70be8d124f84c7cb4a1f453af58d69835
Koushik Saha <Koushik.saha666@gmail.com>
2019-07-06 09:39:49 +0600

    Connect with Firebase Auth, Storage, Database

---
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.whatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
+    }
+}

2f23d0c695c5e816e2e1183aa8135db0152943ff
Koushik Saha <Koushik.saha666@gmail.com>
2019-07-07 23:49:31 +0600

    Fragment with Tabs and ViewPager

--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.whatsapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("WhatsApp");
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
     }
 }

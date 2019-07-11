32162344cc5f5a4abfc0d53d130172a3454a71cc
Koushik Saha <Koushik.saha666@gmail.com>
2019-07-11 11:31:06 +0600

    Login With firebase and fragment design fixed

--- app/src/main/java/com/example/whatsapp/LoginActivity.java
+++ app/src/main/java/com/example/whatsapp/LoginActivity.java
@@ -1,18 +1,29 @@
 package com.example.whatsapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
     private Button LoginButton, PhoneLoginButton;
     private EditText UserEmail, UserPassword;
     private TextView NeedNewAccountLink, ForgetPasswordLink;
@@ -21,6 +32,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
 
         InitializeFields();
@@ -33,6 +47,57 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                AllowUserToLogin();
+            }
+        });
+    }
+
+    private void AllowUserToLogin() {
+
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please Enter Email.....", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please Enter Password....", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+
+            loadingBar.setTitle("Sign In");
+            loadingBar.setMessage("Please wait......");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful())
+                            {
+                                SendUserToMainActivity();
+                                Toast.makeText(LoginActivity.this, "Logged is Successfully.....", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else
+                            {
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
     }
 
     private void InitializeFields() {
@@ -43,6 +108,7 @@ public class LoginActivity extends AppCompatActivity {
         UserPassword = (EditText) findViewById(R.id.login_password);
         NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
         ForgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
+        loadingBar = new ProgressDialog(this);
     }
 
 
--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
@@ -18,11 +19,16 @@ public class MainActivity extends AppCompatActivity {
     private TabsAccessorAdapter myTabsAccessorAdapter;
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);

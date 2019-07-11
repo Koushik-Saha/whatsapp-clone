6963935483cb80408223e4d0f32a8bbcd219e926
Koushik Saha <Koushik.saha666@gmail.com>
2019-07-11 11:53:56 +0600

    Appbar Layout drop down list with Options and Logout button fixed

--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -58,5 +60,39 @@ public class MainActivity extends AppCompatActivity {
 
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_logout_options)
+        {
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if (item.getItemId() == R.id.main_settings_options)
+        {
+
+        }
+        if (item.getItemId() == R.id.main_find_friends_options)
+        {
+
+        }
+
+        return true;
     }
 }

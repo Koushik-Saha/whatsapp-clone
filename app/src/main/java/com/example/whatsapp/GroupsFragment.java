f02cfe69b2c81036d201ad3e4e5fd2d5d08ac930
Koushik Saha <Koushik.saha666@gmail.com>
2019-07-12 09:23:43 +0600

    Pass Data from ListView to Another Activity and show Group name and Group Activity

--- app/src/main/java/com/example/whatsapp/GroupsFragment.java
+++ app/src/main/java/com/example/whatsapp/GroupsFragment.java
@@ -1,11 +1,13 @@
 package com.example.whatsapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -49,6 +51,20 @@ public class GroupsFragment extends Fragment {
         IntializeFields();
 
         RetrieveAndDisplayGroups();
+
+
+        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                String currentGroupName = adapterView.getItemAtPosition(position).toString();
+
+                Intent groupChatIntent = new Intent(getContext(), GroupChatActivity.class);
+                groupChatIntent.putExtra("groupName",currentGroupName);
+                startActivity(groupChatIntent);
+
+            }
+        });
 
         return groupFragmentView;
     }

3fd7cfd06990fda4bd411981d3a880d466dbe22d
Koushik Saha <Koushik.saha666@gmail.com>
2019-07-14 09:25:56 +0600

    Firebase Recycler Adapter onclick item listene and profile activity making and design by user id

--- app/src/main/java/com/example/whatsapp/FindFriendsActivity.java
+++ app/src/main/java/com/example/whatsapp/FindFriendsActivity.java
@@ -1,5 +1,6 @@
 package com.example.whatsapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -61,11 +62,24 @@ public class FindFriendsActivity extends AppCompatActivity {
         FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model)
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, final int position, @NonNull Contacts model)
                     {
                         holder.userName.setText(model.getName());;
                         holder.userStatus.setText(model.getStatus());
                         Picasso.get().load(model.getImage()).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                String visit_user_id = getRef(position).getKey();
+
+                                Intent profileIntent = new Intent(FindFriendsActivity.this,ProfileActivity.class);
+                                profileIntent.putExtra("visit_user_id",visit_user_id);
+                                startActivity(profileIntent);
+                            }
+                        });
                     }
 
                     @NonNull
---
+++ app/src/main/java/com/example/whatsapp/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.example.whatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private String receiverUserID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+        receiverUserID = getIntent().getExtras().get("visit_user_id").toString();
+
+        Toast.makeText(this, "User ID " + receiverUserID, Toast.LENGTH_SHORT).show();
+    }
+}

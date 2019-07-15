31be5d2259df1f5728fd0131259fbf60370cf0b8
Koushik Saha <Koushik.saha666@gmail.com>
2019-07-15 18:51:52 +0600

    Custom Message Layout and Sender Message Layout but Chat Activity Bug

--- app/src/main/java/com/example/whatsapp/ChatsFragment.java
+++ app/src/main/java/com/example/whatsapp/ChatsFragment.java
@@ -30,15 +30,12 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class ChatsFragment extends Fragment
 {
-
     private View PrivateChatsView;
     private RecyclerView chatsList;
 
     private DatabaseReference ChatsRef, UsersRef;
     private FirebaseAuth mAuth;
-    private String currentUserID;
-
-
+    private String currentUserID="";
 
 
     public ChatsFragment() {
@@ -49,7 +46,7 @@ public class ChatsFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        // Inflate the layout for this fragment
         PrivateChatsView = inflater.inflate(R.layout.fragment_chats, container, false);
 
 
@@ -61,6 +58,7 @@ public class ChatsFragment extends Fragment
 
         chatsList = (RecyclerView) PrivateChatsView.findViewById(R.id.chats_list);
         chatsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
 
         return PrivateChatsView;
     }
@@ -74,13 +72,14 @@ public class ChatsFragment extends Fragment
 
         FirebaseRecyclerOptions<Contacts> options =
                 new FirebaseRecyclerOptions.Builder<Contacts>()
-                .setQuery(ChatsRef, Contacts.class)
-                .build();
+                        .setQuery(ChatsRef, Contacts.class)
+                        .build();
 
-        FirebaseRecyclerAdapter<Contacts, ChaatsViewHolder> adapter =
-                new FirebaseRecyclerAdapter<Contacts, ChaatsViewHolder>(options) {
+
+        FirebaseRecyclerAdapter<Contacts, ChatsViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, ChatsViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull final ChaatsViewHolder holder, int position, @NonNull Contacts model)
+                    protected void onBindViewHolder(@NonNull final ChatsViewHolder holder, int position, @NonNull Contacts model)
                     {
                         final String usersIDs = getRef(position).getKey();
                         final String[] retImage = {"default_image"};
@@ -100,17 +99,36 @@ public class ChatsFragment extends Fragment
                                     final String retName = dataSnapshot.child("name").getValue().toString();
                                     final String retStatus = dataSnapshot.child("status").getValue().toString();
 
-
                                     holder.userName.setText(retName);
-                                    holder.userStatus.setText("Last Seen: " + "\n" + "Date " + "Time");
+
+
+                                    if (dataSnapshot.child("userState").hasChild("state"))
+                                    {
+                                        String state = dataSnapshot.child("userState").child("state").getValue().toString();
+                                        String date = dataSnapshot.child("userState").child("date").getValue().toString();
+                                        String time = dataSnapshot.child("userState").child("time").getValue().toString();
+
+                                        if (state.equals("online"))
+                                        {
+                                            holder.userStatus.setText("online");
+                                        }
+                                        else if (state.equals("offline"))
+                                        {
+                                            holder.userStatus.setText("Last Seen: " + date + " " + time);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        holder.userStatus.setText("offline");
+                                    }
 
                                     holder.itemView.setOnClickListener(new View.OnClickListener() {
                                         @Override
-                                        public void onClick(View v)
+                                        public void onClick(View view)
                                         {
                                             Intent chatIntent = new Intent(getContext(), ChatActivity.class);
-                                            chatIntent.putExtra("visit_user_id",usersIDs);
-                                            chatIntent.putExtra("visit_user_name",retName);
+                                            chatIntent.putExtra("visit_user_id", usersIDs);
+                                            chatIntent.putExtra("visit_user_name", retName);
                                             chatIntent.putExtra("visit_image", retImage[0]);
                                             startActivity(chatIntent);
                                         }
@@ -123,15 +141,14 @@ public class ChatsFragment extends Fragment
 
                             }
                         });
-
                     }
 
                     @NonNull
                     @Override
-                    public ChaatsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+                    public ChatsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
                     {
                         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout, viewGroup, false);
-                        return new ChaatsViewHolder(view);
+                        return new ChatsViewHolder(view);
                     }
                 };
 
@@ -140,12 +157,15 @@ public class ChatsFragment extends Fragment
     }
 
 
-    public static class ChaatsViewHolder extends RecyclerView.ViewHolder
+
+
+    public static class  ChatsViewHolder extends RecyclerView.ViewHolder
     {
         CircleImageView profileImage;
         TextView userStatus, userName;
 
-        public ChaatsViewHolder(@NonNull View itemView)
+
+        public ChatsViewHolder(@NonNull View itemView)
         {
             super(itemView);
 
@@ -154,5 +174,4 @@ public class ChatsFragment extends Fragment
             userName = itemView.findViewById(R.id.user_profile_name);
         }
     }
-
 }
